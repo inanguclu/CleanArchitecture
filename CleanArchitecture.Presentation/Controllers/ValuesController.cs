67b86e82a88391a5de33c4479ae6dffceafab158
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-08-17 20:10:01 -0700

    Initial commit

---
+++ CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok("Deneme Api islemi basarili !");
+        }
+    }
+}
---
+++ CleanArchitecture.WebApi/Program.cs
@@ -0,0 +1,39 @@
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Persistance.Context;
+using CleanArchitecture.Persistance.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICarService, CarService>();
+
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+
+builder.Services.AddMediatR(cfr =>
+cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
