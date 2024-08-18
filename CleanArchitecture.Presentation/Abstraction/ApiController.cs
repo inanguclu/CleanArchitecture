40225b64392766aa826d18f674e1f98d230ccdcf
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-08-18 16:02:45 -0700

    -add

--- CleanArchitecture.Presentation/Abstraction/ApiController.cs
+++ CleanArchitecture.Presentation/Abstraction/ApiController.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CleanArchitecture.Presentation.Abstraction
+namespace CleanArchitecture.Presentation.Abstraction;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiController:ControllerBase
 {
-    internal class ApiController
+    private readonly IMediator _mediator;
+
+    protected ApiController(IMediator mediator)
     {
+        _mediator = mediator;
     }
 }
---
+++ CleanArchitecture.Presentation/Controllers/CarsController.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Presentation.Abstraction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    public sealed class CarsController : ApiController
+    {
+        public CarsController(IMediator mediator) : base(mediator){}
+        public async Task<IActionResult> Create(CreateCarCommand)
+        
+    }
+}
