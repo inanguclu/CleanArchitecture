44a20f26195737b7f1875e0d8d271cf25e363280
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2024-08-20 00:58:51 -0700

    add

--- CleanArchitecture.Persistance/obj/Debug/net8.0/CleanArchitecture.Persistance.AssemblyInfo.cs
+++ CleanArchitecture.Persistance/obj/Debug/net8.0/CleanArchitecture.Persistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40225b64392766aa826d18f674e1f98d230ccdcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3c33fb645f683b274d322f380da9076319d48d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
